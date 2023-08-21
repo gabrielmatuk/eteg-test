6457561806c54c464934f4889cb284d9b5dfac88
gabrielmatuk <gabrielmatuk14@gmail.com>
2023-08-21 16:33:17 -0300

    feat(prisma): update missing packages

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,1 +1,16 @@
-//
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+import { PORT } from '../src/config'
+
+const app = express();
+
+app.use(cors());
+
+app.use('/api', routes)
+
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`)
+})
