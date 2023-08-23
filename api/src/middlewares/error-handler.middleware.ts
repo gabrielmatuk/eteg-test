0cec8e55a6229944a268bee3c832816da039268e
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-08-23 12:03:21 -0300

    fix:error handling

--- api/src/middlewares/error-handler.middleware.ts
+++ api/src/middlewares/error-handler.middleware.ts
@@ -3,5 +3,5 @@ import { Request, Response } from 'express';
 import CustomError from '@errors/custom-error';
 
 export const errorHandler = (err: CustomError, _: Request, res: Response) => {
-  res.status(err.status || 500).json({ error: err.message });
+  return res.status(err.status || 502).json({ error: err.message });
 };
