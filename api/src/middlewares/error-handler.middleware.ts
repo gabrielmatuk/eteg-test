26b59bec288450d1fb63737110d7d7b50854e0a2
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-08-23 12:12:37 -0300

    fix: custom error handling

--- api/src/middlewares/error-handler.middleware.ts
+++ api/src/middlewares/error-handler.middleware.ts
@@ -1,7 +1,15 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import CustomError from '@errors/custom-error';
 
-export const errorHandler = (err: CustomError, _: Request, res: Response) => {
-  return res.status(err.status || 502).json({ error: err.message });
+export const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.status || 500).json({ error: err.message });
+  }
+  return res.status(500).json({ error: 'Internal Server Error' });
 };
